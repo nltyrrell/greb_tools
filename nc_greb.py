a810a14f9dada3dafd790d035991fb0a001d0bf3
ntyrrell <nicholas.tyrrell@monash.edu>
2015-02-24 13:43:50 +1100

    nc_greb working

--- nc_greb.py
+++ nc_greb.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import iris
 import iris.plot as iplt
 import iris.quickplot as qplt
+import sys
 import os
 
 # author: Nicholas Loveday
@@ -11,12 +12,13 @@ import os
 
 #########################################################################################
 #Function to read data from GREB 
-def read_data(years, infile, variable, time_length, scenario_name): 
+def read_data(years, infile, variable, scenario_name): 
     xdim  = 96              # x dimensions                  
     ydim  = 48              # y dimensions
     dx    = 3.75            # model resolution
     xydim = xdim*ydim
     tdim  = 12*years        #Total time steps in the data
+    print tdim
     
     
     field = np.zeros((xdim,ydim,tdim)) # Create a zero numpy array with correct dimensions
@@ -25,40 +27,44 @@ def read_data(years, infile, variable, time_length, scenario_name):
     #Process data for plotting
     for n in np.arange(0,tdim):    #Loop through time steps in data
         xin1 = np.fromfile(fid, np.float32, count=xydim) #t_surf
+        print "tdim = "+str(n)
+        print xin1.mean()
         xin2 = np.fromfile(fid, np.float32, count=xydim) #t_atmos
         xin3 = np.fromfile(fid, np.float32, count=xydim) #t_ocean
         xin4 = np.fromfile(fid, np.float32, count=xydim) #vapour 
         xin5 = np.fromfile(fid, np.float32, count=xydim) #albedo
 
         
-    # Populate array with data from the chosen variable
-    if variable == 't_surf':
-        xin = xin1 
-        long_name = 'surface temperature'
-        standard_name = 'surface_temperature'
-        unit = iris.unit.Unit('K')
-    elif variable == 't_atmos':
-        xin = xin2
-        long_name = 'atmosphere temperature'
-        standard_name = 'air_temperature'
-        unit = iris.unit.Unit('K')
-    elif variable == 't_ocean':
-        xin = xin3
-        long_name = 'ocean temperature'
-        standard_name = 'sea_surface_temperature'
-        unit = iris.unit.Unit('K')
-    elif variable == 'vapour':
-        xin = xin4
-        long_name = 'water vapour'
-        standard_name = 'specific_humidity'
-        unit = iris.unit.Unit('kg kg-1')
-    elif variable == 'albedo':
-        xin = xin5                      
-        long_name = 'surface temperature'
-        standard_name = 'surface_albedo'
-        unit = iris.unit.Unit('1')
-    for i in np.arange(0,ydim):
-        field[:,i,n] = xin[(xdim*(i)):((i+1)*xdim)]  
+        # Populate array with data from the chosen variable
+        if variable == 't_surf':
+            xin = xin1 
+            long_name = 'surface temperature'
+            standard_name = 'surface_temperature'
+            unit = iris.unit.Unit('K')
+        elif variable == 't_atmos':
+            xin = xin2
+            long_name = 'atmosphere temperature'
+            standard_name = 'air_temperature'
+            unit = iris.unit.Unit('K')
+        elif variable == 't_ocean':
+            xin = xin3
+            long_name = 'ocean temperature'
+            standard_name = 'sea_surface_temperature'
+            unit = iris.unit.Unit('K')
+        elif variable == 'vapour':
+            xin = xin4
+            long_name = 'water vapour'
+            standard_name = 'specific_humidity'
+            unit = iris.unit.Unit('kg kg-1')
+        elif variable == 'albedo':
+            xin = xin5                      
+            long_name = 'surface temperature'
+            standard_name = 'surface_albedo'
+            unit = iris.unit.Unit('1')
+        print xin.shape
+        print xin.mean()
+        for i in np.arange(0,ydim):
+            field[:,i,n] = xin[(xdim*(i)):((i+1)*xdim)]  
     #Function to flip and rotate data
 
     field = np.rot90(field)
@@ -67,7 +73,7 @@ def read_data(years, infile, variable, time_length, scenario_name):
 
     lons = (np.arange(1.875,360,3.75)) # Define longitudes
     lats = (np.arange(-88.125,90,3.75)) #Define latitutdes
-    time_coord = iris.coords.DimCoord(np.arange(0,600*30,30), standard_name='time', units=iris.unit.Unit('days since 1950-01-15 00:00:0.0', calendar='360_day'))
+    time_coord = iris.coords.DimCoord(np.arange(0,tdim*30,30), standard_name='time', units=iris.unit.Unit('days since 1940-01-15 00:00:0.0', calendar='360_day'))
 
     newcube.add_dim_coord(iris.coords.DimCoord(lats, 'latitude', units='degrees'), 0)
     newcube.add_dim_coord(iris.coords.DimCoord(lons, 'longitude', units='degrees'), 1)
@@ -95,6 +101,7 @@ def read_data(years, infile, variable, time_length, scenario_name):
 # albedo = 'albedo'    
 
 var_list = ['t_surf', 't_atmos', 't_ocean', 'vapour', 'albedo']
+cube_list = iris.cube.CubeList()
 
 #Dimensions
 xdim  = 96
@@ -104,17 +111,19 @@ tstep = 4 #timesteps per day
 xydim = xdim*ydim
 
 #Variables 
-scenario_name = 'clim' #'a1b'
+scenario_name = 'exp5' #'a1b'
 file_name = scenario_name+'.bin' #File name of data to plot
-time_length = 'annual' # Choose out of 'annual', 'DJF', 'MAM', 'JJA', 'SON' and months
 file_size = os.stat(file_name).st_size
 years = file_size/(xdim*ydim*len(var_list)*tstep*12)
 # years = 50 # must match data from GREB model. Choose the number of years that the GREB model is set to run for
 # climate_variable = albedo # Choose climate variable to plot
 
+greb_data, newcube = read_data(years, file_name, 't_surf', scenario_name=scenario_name) # read data
 #read and plot data
+sys.exit('exiiiiiiiiiiiiiiiiiiiiit')
 for i in var_list:
-    greb_data, newcube = read_data(years, file_name, i, time_length, scenario_name=scenario_name) # read data
+    greb_data, newcube = read_data(years, file_name, i, scenario_name=scenario_name) # read data
+    cube_list.append(newcube)
 
 
 
---
+++ write_input.py
@@ -0,0 +1,140 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import iris
+import iris.plot as iplt
+import iris.quickplot as qplt
+import sys
+import os
+
+# author: Nicholas Loveday
+# modified by Nicholas Tyrrell Feb 2015 to use Iris
+# Read in netcdf input files,
+# get data, flatten,
+# Write out binary files
+
+def writebin(infile_nc,outfile_bin):
+    """
+    Write netcdf files to binary, for use with greb model
+    Input: 
+    infile_nc - an iris cube
+    outfile_bin: string, with name of output file, e.g. 'zonal.wind'
+    Output:
+    writes the outfile to disk
+    """
+    dataflat = infile_nc.data.flatten().astype(np.float32)
+    ofile = open('outfile.bin','wb')
+    dataflat.tofile(ofile)
+    ofile.close()
+
+
+infile = iris.load_cube('./ncfiles/meridional.wind.nc')
+
+input_files = ['meridional.wind', 'zonal.wind','tsurf','soil.moisture','vapor','ocean.mld','cloud.cover']
+
+
+sys.exit()
+
+#########################################################################################
+#Function to read data from GREB 
+def read_data(infile, n_tsteps): 
+    
+    field = np.zeros((xdim,ydim,n_tsteps)) # Create a zero numpy array with correct dimensions
+    fid = open(infile,'r') 
+    print infile
+
+    if infile == 'meridional.wind':
+        long_name = 'meridional_wind'
+        standard_name = 'y_wind'
+        unit = iris.unit.Unit('m s-1')
+    elif infile == 'zonal.wind':
+        long_name = 'zonal_wind'
+        standard_name = 'x_wind'
+        unit = iris.unit.Unit('m s-1')
+    elif infile == 'tsurf':
+        long_name = 'surface temperature'
+        standard_name = 'surface_temperature'
+        unit = iris.unit.Unit('K')
+    elif infile == 'cloud.cover':
+        long_name = 'cloud cover'
+        standard_name = 'cloud_area_fraction'
+        unit = iris.unit.Unit('1')
+    elif infile == 'soil.moisture':
+        long_name = 'soil moisture'
+        standard_name = 'soil_moisture_content'
+        unit = iris.unit.Unit('kg m-2')
+    elif infile == 'vapor':
+        long_name = 'water vapor'
+        standard_name = 'specific_humidity'
+        unit = iris.unit.Unit('kg kg-1')
+    elif infile == 'ocean.mld':
+        long_name = 'ocean mld'
+        standard_name = 'ocean_mixed_layer_thickness'
+        unit = iris.unit.Unit('m')
+
+    for n in np.arange(0,n_tsteps):    #Loop through time steps in data
+        xin = np.fromfile(fid, np.float32, count=xydim) #t_surf
+        
+    for i in np.arange(0,ydim):
+        field[:,i,n] = xin[(xdim*(i)):((i+1)*xdim)]  
+        #Function to flip and rotate data
+
+    field = np.rot90(field)
+    field = np.flipud(field) 
+    newcube = iris.cube.Cube(field, long_name=long_name)
+
+    lons = (np.arange(1.875,360,3.75)) # Define longitudes
+    lats = (np.arange(-88.125,90,3.75)) #Define latitutdes
+    time_coord = iris.coords.DimCoord(np.arange(0,n_tsteps), standard_name='time')
+#     time_coord = iris.coords.DimCoord(np.arange(0,600*30,30), standard_name='time', units=iris.unit.Unit('days since 1950-01-15 00:00:0.0', calendar='360_day'))
+#     long_name = 'surface temperature'
+
+    newcube.add_dim_coord(iris.coords.DimCoord(lats, 'latitude', units='degrees'), 0)
+    newcube.add_dim_coord(iris.coords.DimCoord(lons, 'longitude', units='degrees'), 1)
+    newcube.add_dim_coord(time_coord, 2)
+    newcube.standard_name = standard_name
+    newcube.units = unit
+    newcube.attributes = iris._cube_coord_common.LimitedAttributeDict({'source':'Input Data for GREB model'})
+        
+    fid.close()
+
+    iris.save(newcube,'./ncfiles/'+infile+'.nc')
+    return field, newcube
+
+    
+#########################################################################################
+#End of defined funcs   
+#########################################################################################
+#Sample plotting script begins
+##########################################################################################
+#Parameters
+# t_surf = 'surface temperature'
+# t_atmos = 'temperature of atmosphere'
+# t_ocean = 'temperature of ocean'
+# vapour = 'water vapour'
+# albedo = 'albedo'    
+
+# var_list = ['t_surf', 't_atmos', 't_ocean', 'vapour', 'albedo']
+
+#Dimensions
+xdim  = 96
+ydim  = 48
+dx    = 3.75
+tstep = 2 #timesteps per day
+ireal = 4
+xydim = xdim*ydim
+
+#Variables 
+input_files = ['meridional.wind', 'zonal.wind','tsurf','soil.moisture','vapor','ocean.mld','cloud.cover']
+time_length = 'annual' # Choose out of 'annual', 'DJF', 'MAM', 'JJA', 'SON' and months
+# years = 50 # must match data from GREB model. Choose the number of years that the GREB model is set to run for
+# climate_infile = albedo # Choose climate variable to plot
+
+#read and plot data
+for i in input_files:
+    file_size = os.stat(i).st_size
+    n_tsteps = file_size/(xdim*ydim*ireal)
+    greb_data, newcube = read_data(i, n_tsteps)
+
+
+
+
